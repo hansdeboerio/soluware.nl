25e3c626cc48e6b80741d1e81d73167812cad475
hansdeboerio <hans@soluware.nl>
2026-02-15 08:59:52 +0100

    Create config.ts

---
+++ src/lib/config.ts
@@ -0,0 +1,4 @@
+export const SITE_NAME = "Soluware";
+export const SITE_URL = "https://www.soluware.nl";
+export const PORTAL_URL = "https://app.soluware.nl";
+export const SUPPORT_EMAIL = "security@soluware.nl";
